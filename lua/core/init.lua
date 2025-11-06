18d16310f8c0c3736a4256f7703925364d9e3c94
Dysfunctional Coder <136103245+JWLoubser55@users.noreply.github.com>
2025-11-06 10:45:15 +0200

    Change default :term on Windows to pwsh (Powershell 7)

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -12,3 +12,12 @@ vim.cmd [[set nu rnu]]
 vim.g.load_doxygen_syntax = 1
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
+
+if vim.fn.has("win32") == 1 or vim.fn.has("win64") == 1 then
+  if vim.fn.executable("pwsh") == 1 then
+    vim.o.shell = "pwsh"
+    vim.o.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command"
+    vim.o.shellquote = '"'
+    vim.o.shellxquote = ""
+  end
+end
