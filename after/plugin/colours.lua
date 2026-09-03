af604ce84f1f6432cb4db5de974116dcd5d3e491
Dysfunctional Coder <136103245+JWLoubser55@users.noreply.github.com>
2026-09-04 00:37:51 +0200

    Added a new colourscheme and easier switching

--- after/plugin/colours.lua
+++ after/plugin/colours.lua
@@ -1,3 +1,7 @@
+vim.api.nvim_create_user_command("Colour", function(opts)
+    ColourMyPencils(opts.args)
+end, { nargs = 1 })
+
 function ColourMyPencils(colour)
 	colour = colour or "kanagawa"
 	vim.cmd.colorscheme(colour)
